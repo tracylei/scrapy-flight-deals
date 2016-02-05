74a162f11995d727e36561fb9a8280b350d1df69
Tracy Lei <tracylei.yq@gmail.com>
2016-02-05 00:12:28 -0500

    Add geograpy to parse destination countries/cities

--- scrapy_flight_deals/spiders/flight_deals_spider.py
+++ scrapy_flight_deals/spiders/flight_deals_spider.py
@@ -1,7 +1,12 @@
 import scrapy
 from scrapy_flight_deals.items import FlightDealItem
+import geograpy
+
+HOME_CITY 		= 'Toronto'
+HOME_COUNTRY	= 'Canada'
 
 class FlightDealsSpider(scrapy.Spider):
+
 	name = "yyzDeals"
 	allowed_domains = ["yyzdeals.com"]
 	start_urls = ["http://yyzdeals.com/"]
@@ -13,15 +18,32 @@ class FlightDealsSpider(scrapy.Spider):
 			if word[0] == '$':
 				return int(word[1:])
 
+	def parseCountries (self, title):
+		countries 		= geograpy.get_place_context(text=title).countries
+		newCountries 	= []
+		for country in countries:
+			if country != HOME_COUNTRY:
+				newCountries.append(country)
+		return newCountries
+
+	def parseCities (self, title):
+		cities 		= geograpy.get_place_context(text=title).cities
+		newCities 	= []
+		for city in cities:
+			if city != HOME_CITY:
+				newCities.append(city)
+		return newCities
+
 	def parse(self, response):
 		for deal in response.xpath('//div[@id="contentleft"]/h1'):
 			item = FlightDealItem()
-			item['postTitle']	= deal.xpath('a/text()').extract()
-			item['postDay']		= deal.xpath('div[@class="cal"]/span[@class="calday"]/text()').extract()
-			item['postMonth']	= deal.xpath('div[@class="cal"]/span[@class="calmonth"]/text()').extract()
-			item['postYear']	= deal.xpath('div[@class="cal"]/span[@class="calyear"]/text()').extract()
-			item['cost']			= self.parseCost(item['postTitle'])
-			item['destinations']	= item['postTitle']
-			item['link']		= deal.xpath('a/@href').extract()
+			item['postTitle']				= deal.xpath('a/text()').extract()
+			item['postDay']					= deal.xpath('div[@class="cal"]/span[@class="calday"]/text()').extract()
+			item['postMonth']				= deal.xpath('div[@class="cal"]/span[@class="calmonth"]/text()').extract()
+			item['postYear']				= deal.xpath('div[@class="cal"]/span[@class="calyear"]/text()').extract()
+			item['cost']					= self.parseCost(item['postTitle'])
+			item['destinationCountries']	= self.parseCountries(item['postTitle'][0])
+			item['destinationCities']		= self.parseCities(item['postTitle'][0])
+			item['link']					= deal.xpath('a/@href').extract()
 			yield item
 
